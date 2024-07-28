f9dd05ba876f3c8839ad75c7588aeeee24fd3fa3
DeYing Zhang <legendzdy@dingtalk.com>
2024-07-28 10:37:30 +0000

    feat: route

--- frontend/AI-IELTS-Speaking/app.vue
+++ frontend/AI-IELTS-Speaking/app.vue
@@ -3,11 +3,12 @@ useSeoMeta({
   title: 'AI-IELTS-Speaking - 基于 Nuxt.js 的 IELTS 口语评测系统',
   description: 'AI-IELTS-Speaking 是一个基于 Nuxt.js 的 IELTS 口语评测系统，提供视频、文章、词汇、语法、阅读、写作、口语、听力、口语评测等功能。',
 })
-
 </script>
 
 <template>
-  <NuxtPage :keepalive="{ max: 10 }" />
+  <div id="app">
+    <NuxtPage />
+  </div>
 </template>
 
 <style lang="scss">
